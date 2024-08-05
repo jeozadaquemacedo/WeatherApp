ecb4a14edc391fe2e73b280d05d34d7cae9b2a4b
JeozadaqueMacedo <jam6@discente.ifpe.edu.br>
2024-08-05 20:02:35 -0300

    “Prática 02 – Part 2 creating the Navigation Bottons of the main screen ”

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -3,33 +3,72 @@ package com.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.weatherapp.ui.HomePage
+import com.weatherapp.ui.nav.BottomNavBar
+import com.weatherapp.ui.nav.MainNavHost
 import com.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             WeatherAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                    HomePage()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Bem-vindo/a!") },
+                            actions = {
+                                IconButton( onClick = { finish() } ) {
+                                    Icon(
+                                        imageVector = Icons.Default.ExitToApp,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            }
+                        )
+                    },
+                    bottomBar = {
+                        BottomNavBar(navController = navController)
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { }) {
+                            Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                        }
+                    }
+                ) {
+                        innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        MainNavHost(navController = navController)
+                    }
                 }
             }
+
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun HomePagePreview() {
+fun PreviewHomePage() {
     WeatherAppTheme {
         Surface {
             HomePage()
---
+++ app/src/main/java/com/weatherapp/ui/nav/NavHost.kt
@@ -0,0 +1,25 @@
+package com.weatherapp.ui.nav
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.weatherapp.ui.HomePage
+import com.weatherapp.ui.ListPage
+import com.weatherapp.ui.MapPage
+
+@Composable
+fun MainNavHost(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavItem.HomePage.route) {
+        composable(route = BottomNavItem.HomePage.route) {
+            HomePage()
+        }
+        composable(route = BottomNavItem.ListPage.route) {
+            ListPage()
+        }
+        composable(route = BottomNavItem.MapPage.route) {
+            MapPage()
+        }
+    }
+}
+
