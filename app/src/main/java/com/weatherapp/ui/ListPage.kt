bfdd9790b86ea4cf17dbeef9c063a261f711be10
JeozadaqueMacedo <jam6@discente.ifpe.edu.br>
2024-08-06 13:07:17 -0300

    “Prática 03 – Part 1 creating a list of cities ”

--- app/src/main/java/com/weatherapp/ui/ListPage.kt
+++ app/src/main/java/com/weatherapp/ui/ListPage.kt
@@ -1,40 +1,96 @@
 package com.weatherapp.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.weatherapp.R
+import androidx.compose.ui.platform.LocalContext
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview
+data class City(val name: String, var weather: String)
+
+private fun getCities() = List(30) { i ->
+    City(name = "Cidade $i", weather = "Carregando clima...")
+}
+
 @Composable
-fun ListPage() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.purple_200))
-            .wrapContentSize(Alignment.Center)
+fun CityItem(
+    city: City,
+    onClick: () -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth().padding(8.dp).clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = "Favoritas",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+        Icon(
+            Icons.Rounded.FavoriteBorder,
+            contentDescription = ""
         )
+        Spacer(modifier = Modifier.size(12.dp))
+        Column(modifier = modifier.weight(1f)) {
+            Text(modifier = Modifier,
+                text = city.name,
+                fontSize = 24.sp)
+            Text(modifier = Modifier,
+                text = city.weather,
+                fontSize = 16.sp)
+        }
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
     }
 }
 
+@Preview
+@Composable
+fun ListPage(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val cityList = remember { getCities().toMutableStateList() }
+
+    fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        items(cityList) { city ->
+            CityItem(
+                city = city,
+                onClose = {
+                    showToast("Cidade ${city.name} removida")
+                },
+                onClick = {
+                    showToast("Cidade ${city.name} clicada")
+                }
+            )
+        }
+    }
+}
